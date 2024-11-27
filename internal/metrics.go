f46d219e2758261617e666c06279c16db1645aa1
beknur.karshyga <beknur.karshyga@armenotech.com>
2024-11-28 03:24:52 +0500

    metrics added

--- internal/metrics.go
+++ internal/metrics.go
@@ -2,7 +2,11 @@ package internal
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
+	"time"
+
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
 )
 
 const (
@@ -11,17 +15,41 @@ const (
 )
 
 type metricsMiddleware struct {
-	next adapter
-	log  *slog.Logger
+	next    adapter
+	latency *stdprometheus.GaugeVec
+	log     *slog.Logger
 }
 
 func newMetricsMiddleware(next adapter, log *slog.Logger) *metricsMiddleware {
+	stdprometheus.MustRegister(stdServerSLALatency)
+
 	return &metricsMiddleware{
-		next: next,
-		log:  log,
+		next:    next,
+		latency: stdServerSLALatency,
+		log:     log,
 	}
 }
 
+var (
+	stdServerSLALatency = stdprometheus.NewGaugeVec(
+		stdprometheus.GaugeOpts{
+			Namespace: "server",
+			Name:      "http_request_duration_milly_seconds",
+		}, []string{lblService, lblMethod},
+	)
+)
+
 func (m *metricsMiddleware) List(ctx context.Context, query string, args []any) error {
+	start := time.Now()
+
+	defer func() {
+		m.write("List", time.Since(start).Seconds())
+	}()
+
 	return m.next.List(ctx, query, args)
+}
+
+func (m *metricsMiddleware) write(method string, duration float64) {
+	m.log.Info(fmt.Sprintf("spent %v on %s", duration, method))
+	m.latency.WithLabelValues("optimizer", method).Set(duration)
 }
