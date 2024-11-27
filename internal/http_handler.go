7e02296b78e569759f2a2285c4b761ccaa8ec4d8
beknur.karshyga <beknur.karshyga@armenotech.com>
2024-11-28 04:21:11 +0500

    queries added and readme.md update

--- internal/http_handler.go
+++ internal/http_handler.go
@@ -9,20 +9,63 @@ import (
 )
 
 func (s *service) RegisterHandlers(mux *http.ServeMux) {
-	mux.HandleFunc("/list", s.makeFilterHandler())
+	mux.HandleFunc("/first", s.makeGetDataByCountryAndCustomersCountHandler())
+	mux.HandleFunc("/second", s.makeGetDataByCountryAndCityAndCompanyAndCustomersCountHandler())
+	mux.HandleFunc("/third", s.makeGetDataByCompaniesRankAndPastYearsHandler())
 	mux.Handle("/metrics", promhttp.Handler())
 }
 
-func (s *service) makeFilterHandler() http.HandlerFunc {
+// makeGetDataByCountryAndCustomersCountHandler to optimize a huge amount of small queries
+func (s *service) makeGetDataByCountryAndCustomersCountHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var args []any
 
-		args = append(args, r.URL.Query().Get("country"))
-		args = append(args, r.URL.Query().Get("customers_count"))
+		args := []any{
+			r.URL.Query().Get("country"),
+			r.URL.Query().Get("customers_count"),
+		}
 
 		if err := s.next.List(r.Context(), models.Query1, args); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			s.log.Error("cannot handle request", "error", err)
+			s.log.Error("cannot handle Query1 request", "error", err)
+			return
+		}
+
+		w.Write([]byte("OK"))
+	}
+}
+
+// makeGetDataByCountryAndCityAndCompanyAndCustomersCountHandler to optimize a database workload, performance
+func (s *service) makeGetDataByCountryAndCityAndCompanyAndCustomersCountHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		args := []any{
+			r.URL.Query().Get("country"),
+			r.URL.Query().Get("city"),
+			r.URL.Query().Get("company"),
+			r.URL.Query().Get("customers_count"),
+		}
+
+		if err := s.next.List(r.Context(), models.Query2, args); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			s.log.Error("cannot handle Query2 request", "error", err)
+			return
+		}
+
+		w.Write([]byte("OK"))
+	}
+}
+
+// makeGetDataByCompaniesRankAndPastYearsHandler to optimize a database workload, performance but harder than previous
+func (s *service) makeGetDataByCompaniesRankAndPastYearsHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		args := []any{
+			r.URL.Query().Get("country"),
+			r.URL.Query().Get("past_years"),
+			r.URL.Query().Get("rank"),
+		}
+
+		if err := s.next.List(r.Context(), models.Query3, args); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			s.log.Error("cannot handle Query3 request", "error", err)
 			return
 		}
 
--- internal/models/queries.go
+++ internal/models/queries.go
@@ -1,7 +1,8 @@
 package models
 
 const (
-	// Query1 : Pass arguments to filter results
+	// Query1 : Retrieve the country, cities, and customers with at least n customers.
+	//Level1
 	Query1 = `
 SELECT
     country,
@@ -10,12 +11,67 @@ SELECT
 FROM
     customers
 WHERE
-    country = $1 -- Pass the country as a parameter
+    country = $1 
 GROUP BY
     country, city
 HAVING
-    COUNT("Customer Id") >= $2 -- Pass the minimum customers as a parameter
+    COUNT("Customer Id") >= $2
 ORDER BY
     customer_count DESC;
+`
+
+	// Query2 : Retrieve the count of customers grouped by country, city, and company, showing combinations with at least n customers.
+	//Level2
+	Query2 = `
+SELECT 
+    country, 
+    city, 
+    company, 
+    COUNT(*) AS customer_count
+FROM customers
+WHERE country = $1 AND city = $2  AND company = $3
+GROUP BY country, city, company
+HAVING COUNT(*) > $4
+ORDER BY customer_count DESC;
+`
+
+	// Query3 : Retrieve the top n companies in country with the most customers, analyzing their subscription trends over the past m years.
+	//Level3
+	Query3 = `
+WITH subscription_data AS (
+    SELECT 
+        country,
+        company,
+        EXTRACT(YEAR FROM TO_DATE("Subscription Date", 'YYYY-MM-DD')) AS subscription_year,
+        COUNT(*) AS customer_count
+    FROM customers
+    WHERE country = $1
+      AND "Subscription Date" ~ '^\d{4}-\d{2}-\d{2}$'
+      AND EXTRACT(YEAR FROM TO_DATE("Subscription Date", 'YYYY-MM-DD')) >= EXTRACT(YEAR FROM CURRENT_DATE) - $2
+    GROUP BY country, company, EXTRACT(YEAR FROM TO_DATE("Subscription Date", 'YYYY-MM-DD'))
+),
+
+ranked_companies AS (
+    SELECT 
+        country,
+        company,
+        SUM(customer_count) AS total_customers,
+        RANK() OVER (PARTITION BY country ORDER BY SUM(customer_count) DESC) AS rank
+    FROM subscription_data
+    GROUP BY country, company
+)
+
+SELECT 
+    country,
+    company,
+    subscription_year,
+    customer_count
+FROM subscription_data
+WHERE company IN (
+    SELECT company 
+    FROM ranked_companies 
+    WHERE rank <= $3
+)
+ORDER BY country, company, subscription_year;
 `
 )
